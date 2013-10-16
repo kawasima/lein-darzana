d8f3d31b6c5de11a614e1f6098fdae477c42b647
kawasima <kawasima1016@gmail.com>
2013-10-16 13:25:23 +0900

    Fixed darzana.

--- src/leiningen/new/darzana/api.clj
+++ src/leiningen/new/darzana/api.clj
@@ -1,12 +1,15 @@
 (defapi twitter-authorize
   (url "https://api.twitter.com/oauth2/token")
-  (query-keys :grant_type)
+  (query-keys
+    (assign "client_credentials" => :grant_type))
   (basic-auth :consumer-key :consumer-secret)
   (method :post))
 
 (defapi twitter-timeline
   (url "https://api.twitter.com/1.1/search/tweets.json")
-  (query-keys :count :q)
+  (query-keys
+    (assign "clojure" => :q)
+    (assign 10 => :count))
   (expire 300)
   (oauth-token [:twitter-authorize :access_token]))
 
--- src/leiningen/new/darzana/core.clj
+++ src/leiningen/new/darzana/core.clj
@@ -1,7 +1,7 @@
 (ns {{name}}.core
   (:use
     [darzana.core :as darzana]
-    [darzana.api :only (defapi expire url query-keys method basic-auth oauth-token)])
+    [darzana.api])
   (:require
     [compojure.route :as route]
     [compojure.core :as compojure]
@@ -13,15 +13,15 @@
     (darzana.core/set-application-scope
       { :consumer-key nil     ;; Set your twitter consumer key.
         :consumer-secret nil  ;; Set your twitter consumer secret.
-        :q  "clojure"
-        :count 10
-        :grant_type "client_credentials"})
-    (ns {{name}}.core)
+        })
+    (dosync
+      (alter workspace/config assoc :initial-resources "resources"))
     (load-file "resources/api.clj")
-    (darzana/add-routes "routes")
-    (darzana/load-routes)
-    (reload/wrap-reload (->
-                          (handler/site (compojure/routes
-                                          darzana/routes
-                                          (compojure/context "/admin" [] darzana/admin-routes)
-                                          (route/resources "/")))))))
+    (workspace/change-workspace "master")
+    (reload/wrap-reload
+      (->
+        (handler/site
+          (compojure/routes
+            (darzana/load-routes)
+            (compojure/context "/admin" [] darzana/admin-routes)
+            (route/resources "/")))))))
